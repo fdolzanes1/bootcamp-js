a92f1fd14ac81a55abafab3f5a23c7e54efe31e1
fdolzanes1 <fdolzanes1@gmail.com>
2021-03-08 20:02:17 -0400

    added project creating elements

---
+++ 05-document-dom/proj03-criando-elementos/script.js
@@ -0,0 +1,37 @@
+let fsList = document.querySelectorAll('.multiple-field');
+
+for(let i=0; i<fsList.length; i++) {
+	initMultipleFieldSet(fsList[i]);
+}
+
+function initMultipleFieldSet(fs) {
+	let addButton = document.createElement("button");
+	addButton.textContent = "Adicionar";
+	addButton.type = "button";
+	fs.appendChild(addButton);
+
+	let firstInput = fs.querySelector("input");
+
+	addButton.addEventListener('click', function(){
+		let newInput = document.createElement("input");
+		newInput.name = firstInput.name;
+		newInput.type = firstInput.type;
+
+
+		let deleteButton = document.createElement("button");
+		deleteButton.textContent = "Excluir";
+		deleteButton.type = "button";
+
+		
+
+		var div = document.createElement("div");
+		div.appendChild(newInput);
+		div.appendChild(deleteButton);
+
+		deleteButton.addEventListener('click', function() {
+			div.remove();
+		});
+
+		fs.insertBefore(div, addButton);
+	});
+}
