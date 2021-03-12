e65cefaa16b8c3bcf295b08e56f1c558417f4ce4
fdolzanes1 <fdolzanes1@gmail.com>
2021-03-12 09:30:37 -0400

    instanciando class person

--- 06-oob-js/a01-instanciando-objetos/script.js
+++ 06-oob-js/a01-instanciando-objetos/script.js
@@ -13,3 +13,23 @@ function area() {
 function perimetro() {
 	return this.altura * 2 + this.largura * 2;
 }
+
+function Person(first, last, age, gender, job) {
+	this.name = {
+		'first': first, 
+		'last': last
+	};
+
+	this.age = age; 
+	this.gender = gender; 
+	this.job = job; 
+
+	this.bio = function() {
+		console.log(this.name.first+' '+this.name.last+' is '+this.age+' years old. He is a '+this.job);
+	}
+
+	this.greeting = function() {
+		console.log("Hi! I'm "+this.name.first+".");
+	}
+
+}
